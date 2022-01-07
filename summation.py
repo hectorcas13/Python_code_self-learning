87e66847c2c0f75aff2450fe2130c8cbc1e5bcb3
Héctor Castillo <49953794+hectorcas13@users.noreply.github.com>
2022-01-07 00:30:05 -0500

    Update summation.py

--- summation.py
+++ summation.py
@@ -2,14 +2,14 @@
 # it takes a range of two values to do a summation for example:
 # [1,3] = 1+2+3 = 6
 
-l = [2,10]
+l = [3,8]
 ini = l[0] 
 fin = l[1]
 ini2 = 0
 cal2 = 0
 if ini != 1:
     ini2= ini-1
-    for i in range(1,2):cal2 = (ini2 + 1) * ini2 / 2  
+    for i in range(len(l)):cal2 = (ini2 + 1) * ini2 / 2  
     for i in range(ini,fin): cal1 = (fin + 1) * fin / 2 
     print(int(cal1-cal2))
 else:
